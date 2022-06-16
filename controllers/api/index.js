071b8b9bd4f5c326aa49664eea0a25e1c17c787b
Will G <wgrout87@gmail.com>
2022-06-16 10:48:30 -0600

    added outline routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,9 +1,9 @@
 const router = require("express").Router();
 
 const userRoutes = require("./user-routes");
-// const outlineRoutes = require("./outline-routes");
+const outlineRoutes = require("./outline-routes");
 
 router.use("/users", userRoutes);
-// router.use("/outlines", outlineRoutes);
+router.use("/outlines", outlineRoutes);
 
 module.exports = router;
---
+++ controllers/api/outline-routes.js
@@ -0,0 +1,88 @@
+const router = require("express").Router();
+const { Outline } = require("../../models");
+
+// GET /api/outlines - gets all outlines in the database
+router.get("/", (req, res) => {
+  // Returns all outlines in the database
+  Outline.findAll({})
+    // Returns the outline info for all outlines
+    .then((outlineData) => res.json(outlineData))
+    // Basic error catching
+    .catch((err) => res.status(500).json(err));
+});
+
+// GET /api/outlines/:id - gets a specific outline in the database
+router.get("/:id", (req, res) => {
+  // Returns a specific outline from the database
+  Outline.findOne({
+    // Specifies the primary key of the desired outline
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((outlineData) => {
+      // Checks if the outline at the specified ID exists - sends an error if they don't
+      if (!outlineData) {
+        res.status(404).json({ message: "No outline found with this id" });
+        return;
+      }
+      // Returns the outline info
+      res.json(outlineData);
+    })
+    // Basic error catching
+    .catch((err) => res.status(500).json(err));
+});
+
+// POST /api/outlines - adds a new outline to the database
+router.post("/", (req, res) => {
+  // req.body is used because the body of the JSON object being passed through the route should have all the pertinent information to create a new outline (outlinename and password)
+  Outline.create(req.body)
+    .then((outlineData) => res.json(outlineData))
+    // Basic error catching
+    .catch((err) => res.status(500).json(err));
+});
+
+// PUT /api/outlines/:id - updates a specific outline in the database
+router.put("/:id", (req, res) => {
+  // req.body is used because the body of the JSON object being passed through the route should have any pertinent information to update the outline (outlinename, password, or both)
+  Outline.update(req.body, {
+    // Indicates which outline to update
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((outlineData) => {
+      // Checks if the outline at the specified ID exists - sends an error if they don't
+      if (!outlineData[0]) {
+        res.status(404).json({ message: "No outline found with this id" });
+        return;
+      }
+      // Will return the updated outline info
+      res.json(outlineData);
+    })
+    // Basic error catching
+    .catch((err) => res.status(500).json(err));
+});
+
+// DELETE /api/outlines/:id - deletes a specific outline in the database
+router.delete("/:id", (req, res) => {
+  Outline.destroy({
+    // Indicates which outline to update
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((outlineData) => {
+      // Checks if the outline at the specified ID exists - sends an error if they don't
+      if (!outlineData) {
+        res.status(404).json({ message: "No outline found with this id" });
+        return;
+      }
+      // Will return the number of changed rows
+      res.json(outlineData);
+    })
+    // Basic error catching
+    .catch((err) => res.status(500).json(err));
+});
+
+module.exports = router;
