d7ec5cae17eec473f4e26d1359aad2b00fe61dda
Will G <wgrout87@gmail.com>
2022-06-21 15:52:34 -0600

    added delete and view functionality to dashboard buttons

--- controllers/index.js
+++ controllers/index.js
@@ -2,10 +2,12 @@ const router = require("express").Router();
 
 const homeRoutes = require("./home-routes");
 const dashboardRoutes = require("./dashboard-routes");
+const outlineRoutes = require("./outline-routes");
 const apiRoutes = require("./api");
 
 router.use("/", homeRoutes);
 router.use("/dashboard", dashboardRoutes);
+router.use("/outline", outlineRoutes);
 router.use("/api", apiRoutes);
 
 router.use((req, res) => {
---
+++ public/javascript/dashboard.js
@@ -0,0 +1,43 @@
+// Function for deleting an outline
+async function deleteOutline(event) {
+  event.preventDefault();
+
+  // Checks that the event target was a delete button
+  if (!event.target.classList.contains("deleteOutline")) {
+    return;
+  }
+
+  // Defines the URL to use for fetching using the data- variable containing the outline ID
+  const fetchUrl = "/api/outlines/" + event.target.dataset.id;
+
+  // Sends a fetch request to delete the outline
+  const response = await fetch(fetchUrl, {
+    method: "DELETE",
+  });
+
+  // Reloads the page if the outline was successfully deleted - the outline will no longer display
+  if (response.ok) {
+    document.location.reload();
+  } else {
+    alert(response.statusText);
+  }
+}
+
+function viewOutline(event) {
+  event.preventDefault();
+
+  // Checks that the event target was a view button
+  if (!event.target.classList.contains("viewOutline")) {
+    return;
+  }
+
+  document.location.replace("/outline/" + event.target.dataset.id);
+}
+
+// Adds event listeners to the outline section to handle deleting and editing outlines
+document
+  .querySelector("#outlineSection")
+  .addEventListener("click", deleteOutline);
+document
+  .querySelector("#outlineSection")
+  .addEventListener("click", viewOutline);
