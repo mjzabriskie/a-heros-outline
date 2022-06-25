f1c9dc2ee4508a94f5b89d12b6fcc5d94c4fb0d5
mjzabriskie <99937908+mjzabriskie@users.noreply.github.com>
2022-06-24 21:35:51 -0600

    added new functions for wizard, made progress on debugging

--- public/javascript/outline.js
+++ public/javascript/outline.js
@@ -93,6 +93,63 @@ function saveButtonHandler(event) {
     : saveNewOutline(event);
 }
 
+let currStep = 0; // Current tab is set to be the first tab (0)
+showStep(currStep); // Display the current tab
+
+function showStep(n) {
+  // This function will display the specified tab of the form...
+  var x = document.getElementsByClassName("tab");
+
+  x[n].style.display = "block";
+  //... and fix the Previous/Next buttons:
+  if (n == 0) {
+    document.getElementById("prevBtn").style.display = "none";
+  } else {
+    document.getElementById("prevBtn").style.display = "inline";
+  }
+  if (n == (x.length - 1)) {
+    document.getElementById("nextBtn").textContent = "Finish";
+  } else {
+    document.getElementById("nextBtn").textContent = "Next";
+  }
+  //... and run a function that will display the correct step indicator:
+  fixStepIndicator(n)
+}
+
+function nextPrev(n) {
+  // This function will figure out which tab to display
+  var x = document.getElementsByClassName("tab");
+  // Exit the function if any field in the current tab is invalid:
+  if (n == 1 && !validateForm()) return false;
+  // Hide the current tab:
+  x[currStep].style.display = "none";
+  // Increase or decrease the current tab by 1:
+  currStep = currStep + n;
+  // if you have reached the end of the form...
+  if (currStep >= x.length) {
+    // ... the form gets submitted:
+    document.getElementById("regForm").submit();
+    return false;
+  }
+  // Otherwise, display the correct tab:
+  showStep(currStep);
+}
+
+function validateForm() {
+    document.getElementsByClassName("step")[currStep].className += " finish";
+  return true; // return the valid status
+}
+
+function fixStepIndicator(n) {
+  // This function removes the "active" class of all steps...
+  var i, x = document.getElementsByClassName("step");
+  for (i = 0; i < x.length; i++) {
+    x[i].className = x[i].className.replace(" active", "");
+  }
+  //... and adds the "active" class on the current step:
+  x[n].className += " active";
+}
+
 document
   .querySelector("#outlineSection")
   .addEventListener("click", clearTextarea);
