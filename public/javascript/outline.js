5e3755137e76a3c36a1a433b2b286f51214fb55e
mjzabriskie <99937908+mjzabriskie@users.noreply.github.com>
2022-06-26 03:20:37 -0600

    cleaned up the code for the outline page, standardized naming based on programming language. Made some comments

--- public/javascript/outline.js
+++ public/javascript/outline.js
@@ -1,113 +1,84 @@
+//Forms
 const outlineFormEl = document.querySelector("#outline-form");
 const wizardFormEl = document.querySelector("#wizard-form");
-const prevBtn = document.querySelector("#prevBtn");
-const comfortZoneEl = document.querySelector("#comfortZone");
-const comfortZoneWizEl = document.querySelector("#comfortZoneWiz");
-const characterDesireEl = document.querySelector("#characterDesire");
-const characterDesireWizEl = document.querySelector("#characterDesireWiz");
-const newSituationEl = document.querySelector("#newSituation");
-const newSituationWizEl = document.querySelector("#newSituationWiz");
-const characterAdaptsEl = document.querySelector("#characterAdapts");
-const characterAdaptsWizEl = document.querySelector("#characterAdaptsWiz");
-const getsDesireEl = document.querySelector("#getsDesire");
-const getsDesireWizEl = document.querySelector("#getsDesireWiz");
-const heavyPriceEl = document.querySelector("#heavyPrice");
-const heavyPriceWizEl = document.querySelector("#heavyPriceWiz");
-const familiarSituationEl = document.querySelector("#familiarSituation");
-const familiarSituationWizEl = document.querySelector("#familiarSituationWiz");
-const characterChangedEl = document.querySelector("#characterChanged");
-const characterChangedWizEl = document.querySelector("#characterChangedWiz");
 
-comfortZoneEl.addEventListener('change', function () {
-  comfortZoneWizEl.value = this.value;
-});
-comfortZoneWizEl.addEventListener('change', function () {
-  comfortZoneEl.value = this.value;
-});
+//Buttons
+const prevBtnEl = document.querySelector("#prev-btn");
+const nextBtnEl = document.querySelector("#next-btn");
+const saveOutlineBtnEl = document.querySelector("#save-outline-btn");
+const wizardBtnEl = document.querySelector("#wizard-btn");
+const exitBtnEl = document.querySelector("#exit-btn");
 
-characterDesireEl.addEventListener('change', function () {
-  characterDesireWizEl.value = this.value;
-});
-characterDesireWizEl.addEventListener('change', function () {
-  characterDesireEl.value = this.value;
-});
+//Textareas
+const outlineTitleEl = document.querySelector("#outline-title");
+const outlineTitleWizEl = document.querySelector("#outline-title-wiz");
+const comfortZoneEl = document.querySelector("#comfort-zone");
+const comfortZoneWizEl = document.querySelector("#comfort-zone-wiz");
+const characterDesireEl = document.querySelector("#character-desire");
+const characterDesireWizEl = document.querySelector("#character-desire-wiz");
+const newSituationEl = document.querySelector("#new-situation");
+const newSituationWizEl = document.querySelector("#new-situation-wiz");
+const characterAdaptsEl = document.querySelector("#character-adapts");
+const characterAdaptsWizEl = document.querySelector("#character-adapts-wiz");
+const getsDesireEl = document.querySelector("#gets-desire");
+const getsDesireWizEl = document.querySelector("#gets-desire-wiz");
+const heavyPriceEl = document.querySelector("#heavy-price");
+const heavyPriceWizEl = document.querySelector("#heavy-price-wiz");
+const familiarSituationEl = document.querySelector("#familiar-situation");
+const familiarSituationWizEl = document.querySelector("#familiar-situation-wiz");
+const characterChangedEl = document.querySelector("#character-changed");
+const characterChangedWizEl = document.querySelector("#character-changed-wiz");
 
-newSituationEl.addEventListener('change', function () {
-  newSituationWizEl.value = this.value;
-});
-newSituationWizEl.addEventListener('change', function () {
-  newSituationEl.value = this.value;
-});
+//Controller input variables
+let title;
+let comfort_zone;
+let character_desire;
+let new_situation;
+let character_adapts;
+let gets_desire;
+let heavy_price;
+let familiar_situation;
+let character_changed;
 
-characterAdaptsEl.addEventListener('change', function () {
-  characterAdaptsWizEl.value = this.value;
-});
-characterAdaptsWizEl.addEventListener('change', function () {
-  characterAdaptsEl.value = this.value;
-});
+//Function variables
+let currStep = 0;
 
-getsDesireEl.addEventListener('change', function () {
-  getsDesireWizEl.value = this.value;
-});
-getsDesireWizEl.addEventListener('change', function () {
-  getsDesireEl.value = this.value;
-});
-
-heavyPriceEl.addEventListener('change', function () {
-  heavyPriceWizEl.value = this.value;
-});
-heavyPriceWizEl.addEventListener('change', function () {
-  heavyPriceEl.value = this.value;
-});
-
-familiarSituationEl.addEventListener('change', function () {
-  familiarSituationWizEl.value = this.value;
-});
-familiarSituationWizEl.addEventListener('change', function () {
-  familiarSituationEl.value = this.value;
-});
-
-characterChangedEl.addEventListener('change', function () {
-  characterChangedWizEl.value = this.value;
-});
-characterChangedWizEl.addEventListener('change', function () {
-  characterChangedEl.value = this.value;
-});
-
-
+//Looks for query string to determine which view opens on initialize
 if (window.location.search) {
   wizardFormEl.classList.toggle("d-none");
 } else {
   outlineFormEl.classList.toggle("d-none");
 }
 
+// Checks that the event target was a clear button
 function clearTextarea(event) {
-  // Checks that the event target was a clear button
   if (!event.target.classList.contains("clear-btn")) {
     return;
   }
-
+  //finds the textarea in the parent of the button
   event.target.parentElement.querySelector("textarea").value = "";
 }
 
+//Determines which fetch request will be completed, PUT or POST
+function saveButtonHandler(event) {
+  return event.target.dataset.id
+    ? saveOldOutline(event)
+    : saveNewOutline(event);
+}
+
+//Uses POST to create new outline in database
 async function saveNewOutline(event) {
   event.preventDefault();
 
-  const title = document.querySelector("#outline-title").value.trim();
-  const comfort_zone =
-    document.querySelector("#comfortZone").value.trim() || "";
-  const character_desire =
-    document.querySelector("#characterDesire").value.trim() || "";
-  const new_situation =
-    document.querySelector("#newSituation").value.trim() || "";
-  const character_adapts =
-    document.querySelector("#characterAdapts").value.trim() || "";
-  const gets_desire = document.querySelector("#getsDesire").value.trim() || "";
-  const heavy_price = document.querySelector("#heavyPrice").value.trim() || "";
-  const familiar_situation =
-    document.querySelector("#familiarSituation").value.trim() || "";
-  const character_changed =
-    document.querySelector("#characterChanged").value.trim() || "";
+  title = outlineTitleEl.value.trim() || "";
+  comfort_zone = comfortZoneEl.value.trim() || "";
+  character_desire = characterDesireEl.value.trim() || "";
+  new_situation = newSituationEl.value.trim() || "";
+  character_adapts = characterAdaptsEl.value.trim() || "";
+  gets_desire = getsDesireEl.value.trim() || "";
+  heavy_price = heavyPriceEl.value.trim() || "";
+  familiar_situation = familiarSituationEl.value.trim() || "";
+  character_changed = characterChangedEl.value.trim() || "";
 
   if (title) {
     try {
@@ -129,24 +100,19 @@ async function saveNewOutline(event) {
   }
 }
 
+//Uses PUT to update existing outlines
 async function saveOldOutline(event) {
   event.preventDefault();
 
-  const title = document.querySelector("#outline-title").value.trim();
-  const comfort_zone =
-    document.querySelector("#comfortZone").value.trim() || "";
-  const character_desire =
-    document.querySelector("#characterDesire").value.trim() || "";
-  const new_situation =
-    document.querySelector("#newSituation").value.trim() || "";
-  const character_adapts =
-    document.querySelector("#characterAdapts").value.trim() || "";
-  const gets_desire = document.querySelector("#getsDesire").value.trim() || "";
-  const heavy_price = document.querySelector("#heavyPrice").value.trim() || "";
-  const familiar_situation =
-    document.querySelector("#familiarSituation").value.trim() || "";
-  const character_changed =
-    document.querySelector("#characterChanged").value.trim() || "";
+  title = outlineTitleEl.value.trim() || "";
+  comfort_zone = comfortZoneEl.value.trim() || "";
+  character_desire = characterDesireEl.value.trim() || "";
+  new_situation = newSituationEl.value.trim() || "";
+  character_adapts = characterAdaptsEl.value.trim() || "";
+  gets_desire = getsDesireEl.value.trim() || "";
+  heavy_price = heavyPriceEl.value.trim() || "";
+  familiar_situation = familiarSituationEl.value.trim() || "";
+  character_changed = characterChangedEl.value.trim() || "";
 
   const fetchUrl = "/api/outlines/" + event.target.dataset.id;
 
@@ -170,39 +136,50 @@ async function saveOldOutline(event) {
   }
 }
 
-function saveButtonHandler(event) {
-  return event.target.dataset.id
-    ? saveOldOutline(event)
-    : saveNewOutline(event);
+//Shows the whichever step number is passed in
+function showStep(step) {
+  const tabsArray = document.getElementsByClassName("tab");
+
+  tabsArray[step].style.display = "block";
+
+  //Controls button visiblity and textContent depending on the step
+  if (step == 0) {
+    document.getElementById("prev-btn").style.display = "none";
+  } else {
+    document.getElementById("prev-btn").style.display = "inline";
+  }
+  if (step == tabsArray.length - 1) {
+    document.getElementById("next-btn").textContent = "Finish";
+  } else {
+    document.getElementById("next-btn").textContent = "Next";
+  }
+  //calls function to set step indicator
+  setStepIndicator(step);
 }
 
-let currStep = 0; // Current tab is set to be the first tab (0)
-showStep(currStep); // Display the current tab
-
-function showStep(n) {
-  // This function will display the specified tab of the form...
-  var x = document.getElementsByClassName("tab");
-
-  x[n].style.display = "block";
-  //... and fix the Previous/Next buttons:
-  if (n == 0) {
-    document.getElementById("prevBtn").style.display = "none";
-  } else {
-    document.getElementById("prevBtn").style.display = "inline";
+//Sets step indicator
+function setStepIndicator(step) {
+  // This function removes the "active" class of all steps...
+  let i;
+  const stepsArray = document.getElementsByClassName("step");
+  for (i = 0; i < stepsArray.length; i++) {
+    stepsArray[i].className = stepsArray[i].className.replace(" active", "");
   }
-  if (n == x.length - 1) {
-    document.getElementById("nextBtn").textContent = "Finish";
-  } else {
-    document.getElementById("nextBtn").textContent = "Next";
-  }
-  //... and run a function that will display the correct step indicator:
-  fixStepIndicator(n);
+  //adds active class on current step
+  stepsArray[step].className += " active";
 }
 
+//controls movement from tab to tab
 function nextPrev(event) {
   let n;
 
-  if (event.target.id == "nextBtn") {
+  //Stops user from continuing past number of tabs
+  if (nextBtnEl.textContent === "Finish" && event.target.textContent === "Finish") {
+    wizardToggle();
+    return;
+  }
+
+  if (event.target.id == "next-btn") {
     n = 1;
   } else {
     n = -1;
@@ -231,29 +208,82 @@ function validateForm() {
   return true; // return the valid status
 }
 
-function fixStepIndicator(n) {
-  // This function removes the "active" class of all steps...
-  var i,
-    x = document.getElementsByClassName("step");
-  for (i = 0; i < x.length; i++) {
-    x[i].className = x[i].className.replace(" active", "");
-  }
-  //... and adds the "active" class on the current step:
-  x[n].className += " active";
-}
-
 function wizardToggle() {
   wizardFormEl.classList.toggle("d-none");
   outlineFormEl.classList.toggle("d-none");
 }
 
-document
-  .querySelector("#outlineSection")
-  .addEventListener("click", clearTextarea);
-document
-  .querySelector("#singlebutton")
-  .addEventListener("click", saveButtonHandler);
-document.querySelector("#nextBtn").addEventListener("click", nextPrev);
-prevBtn.addEventListener("click", nextPrev);
-document.querySelector("#wizard-btn").addEventListener("click", wizardToggle);
-document.querySelector("#exit-btn").addEventListener("click", wizardToggle);
+showStep(currStep);
+
+outlineFormEl.addEventListener("click", clearTextarea);
+saveOutlineBtnEl.addEventListener("click", saveButtonHandler);
+nextBtnEl.addEventListener("click", nextPrev);
+prevBtnEl.addEventListener("click", nextPrev);
+wizardBtnEl.addEventListener("click", wizardToggle);
+exitBtnEl.addEventListener("click", wizardToggle);
+
+//The following listeners synchronize changes between outline view and wizard view
+outlineTitleEl.addEventListener("change", function () {
+  outlineTitleWizEl.value = this.value;
+});
+if (outlineTitleWizEl){//handlebars helper determins if this element is created
+  outlineTitleWizEl.addEventListener("change", function () {
+    outlineTitleEl.value = this.value;
+  });
+}
+
+comfortZoneEl.addEventListener("change", function () {
+  comfortZoneWizEl.value = this.value;
+});
+comfortZoneWizEl.addEventListener("change", function () {
+  comfortZoneEl.value = this.value;
+});
+
+characterDesireEl.addEventListener("change", function () {
+  characterDesireWizEl.value = this.value;
+});
+characterDesireWizEl.addEventListener("change", function () {
+  characterDesireEl.value = this.value;
+});
+
+newSituationEl.addEventListener("change", function () {
+  newSituationWizEl.value = this.value;
+});
+newSituationWizEl.addEventListener("change", function () {
+  newSituationEl.value = this.value;
+});
+
+characterAdaptsEl.addEventListener("change", function () {
+  characterAdaptsWizEl.value = this.value;
+});
+characterAdaptsWizEl.addEventListener("change", function () {
+  characterAdaptsEl.value = this.value;
+});
+
+getsDesireEl.addEventListener("change", function () {
+  getsDesireWizEl.value = this.value;
+});
+getsDesireWizEl.addEventListener("change", function () {
+  getsDesireEl.value = this.value;
+});
+
+heavyPriceEl.addEventListener("change", function () {
+  heavyPriceWizEl.value = this.value;
+});
+heavyPriceWizEl.addEventListener("change", function () {
+  heavyPriceEl.value = this.value;
+});
+
+familiarSituationEl.addEventListener("change", function () {
+  familiarSituationWizEl.value = this.value;
+});
+familiarSituationWizEl.addEventListener("change", function () {
+  familiarSituationEl.value = this.value;
+});
+
+characterChangedEl.addEventListener("change", function () {
+  characterChangedWizEl.value = this.value;
+});
+characterChangedWizEl.addEventListener("change", function () {
+  characterChangedEl.value = this.value;
+});
